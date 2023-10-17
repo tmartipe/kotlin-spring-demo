ffbe98bbc0d5a73dca3bb6ae1da8a59ce3252e21
Tomas Martinez <tomasurielmp@gmail.com>
2023-10-17 10:05:16 -0300

    Compose and mongo configurations

--- src/main/kotlin/com/example/endpointsdemo/EndpointsDemoApplication.kt
+++ src/main/kotlin/com/example/endpointsdemo/EndpointsDemoApplication.kt
@@ -2,10 +2,12 @@ package com.example.endpointsdemo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Profile
 
 @SpringBootApplication
+@Profile("!docker-compose")
 class EndpointsDemoApplication
 
 fun main(args: Array<String>) {
-	runApplication<EndpointsDemoApplication>(*args)
+    runApplication<EndpointsDemoApplication>(*args)
 }
--- src/main/kotlin/com/example/endpointsdemo/controllers/DemoRestController.kt
+++ src/main/kotlin/com/example/endpointsdemo/controllers/DemoRestController.kt
@@ -1,4 +1,14 @@
 package com.example.endpointsdemo.controllers
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
 class DemoRestController {
+
+    @GetMapping("/greeting")
+    fun greeting() : String {
+        return "Hola! Gracias por probar la demo! :)"
+    }
+
 }
